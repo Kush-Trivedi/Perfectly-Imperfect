8fb134eb1bd7bbffd4bc91c03787bf67736da21f
Kush Trivedi <kushtrivedi14728@gmail.com>
2024-12-28 10:53:17 -0600

    Initial Commit

--- views/introduction.py
+++ views/introduction.py
@@ -2,4 +2,38 @@ import streamlit as st
 
 st.title(":violet[NFL] Big Data Bowl 2025")
 
-st.write("In-Progress.....")
+st.markdown(
+    """
+    #### Introduction
+    Please allow me to intoduce myself I am *Kush Trivedi a Sr.ML/Data Scientist* my journey in analyzing NFL Big Data Bowl over the past few years has taught me a valuable lesson.
+    Despite my technical expertise & enthusiasm towards football, I always lacked the crucial domain knowledge of football strategies and their execution on the field. 
+    Moreover, this year, I am very thankful to **DFO** ([**DeMarkco Butler**](https://ucmathletics.com/staff-directory/demarkco-butler/388)) for all his support throughout this project & 
+    I truly appreciate the live game valuable insights from [**Coach Josh Lenberson’s**](https://themiaa.com/news/2023/12/6/football-josh-lamberson-named-afca-division-ii-super-region-coach-of-the-year.aspx), who is the ***AFCA Super Region III Coach of the Year 2023*** and the ***2023 MIAA Coach of the Year***.
+
+    As NFL offenses continue to innovate in the face of aggressive defensive schemes, short-passing plays have emerged as the cornerstone of modern strategy. 
+    With defenses emphasizing pressure and coverage schemes to limit deep balls, offenses are responding by capitalizing on quick, efficient **Passes—DINKING** and **DUNKING** their way to dominance.
+
+    **The numbers back it up**: Short passes, particularly to the *middle*, *left*, and *right* have consistently shown higher success rates in sustaining drives and avoiding costly turnovers. 
+    Teams that master these plays gain an edge in controlling the *clock*, converting key *downs*, and maximizing *Expected Points Added (EPA)* per play. **The numbers back it up! → For real? → Yes**
+
+    
+    - But how do teams achieve this precision? 
+    - What drives the success of short passes in different scenarios? 
+    - How pre-snap formations and tendencies could predict post-snap outcomes?
+   
+    This project provides answers to such questions by visualizes the *trends*, analyzing *quaterbacks-receivers patterns*, *strategies*, hands on with ***real time game play scenario simulator*** & revealing not only what happens but also why.
+    
+    """
+)
+
+st.markdown("""
+    #### Playground
+
+    1. **Dink & Dunk Report**: A handy report that gives the ***historical trends***, ***tendencies***, & ***routes-pass patterns*** for 32 NFL Teams.
+    2. **Game Play Simulator**: A real time game play simulator where you can provide any game play scenarios and get **88%-93%** accurate predicted data-driven decisions on player performance, predicted yard gain outcomes, predicted offensive strategies & much more.
+    3. **QB's Radar**: This model help quarterbacks **identify the best receiver** for a  **Deep Passes** play by using tracking data with **video animation**.
+    
+    """
+)
+
+
